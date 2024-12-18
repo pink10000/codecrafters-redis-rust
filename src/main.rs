b9cc4edb8d9ca53e324893fdae0d2ca6b4c7b1c1
pink10000 <kyletrinh1223@gmail.com>
2024-12-18 13:32:48 -0800

    pong loop

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::{TcpListener, TcpStream}};
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
-fn handle_client(mut stream: TcpStream) {
-
+fn handle_client(stream: &mut TcpStream) {
     let pong_str: String = "+PONG\r\n".to_string();
     let _ = stream.write(pong_str.as_bytes());
-    
 }
 
-
-
-fn main() {    
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                handle_client(_stream);
-
-
-
+                while _stream.peek(&mut [0; 1]).is_ok() {
+                    handle_client(&mut _stream);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
