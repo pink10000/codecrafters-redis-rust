038c66caaebdbafabb54ea1621c0448c76c58816
pink10000 <kyletrinh1223@gmail.com>
2024-12-18 13:16:55 -0800

    hard coded PONG

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::{TcpListener, TcpStream}};
+
+fn handle_client(mut stream: TcpStream) {
+
+    let pong_str: String = "+PONG\r\n".to_string();
+    let _ = stream.write(pong_str.as_bytes());
+    
+}
+
+
 
 fn main() {    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                handle_client(_stream);
+
+
+
             }
             Err(e) => {
                 println!("error: {}", e);
