2b2c7b47ddbb459f6496a999bde613e73c08dd2a
pink10000 <kyletrinh1223@gmail.com>
2024-12-20 12:34:25 -0800

    len check 2 to 3

--- src/server.rs
+++ src/server.rs
@@ -50,7 +50,7 @@ impl ServerState {
     fn handle_set(&mut self, arr: Vec<RespType>) -> String {
         let key = self.execute_resp(arr[1].clone());
         let value = self.execute_resp(arr[2].clone());
-        if arr.len() == 2 {
+        if arr.len() == 3 {
             self.db.insert(key.clone(), value);
             return "+OK\r\n".to_string();
         }
