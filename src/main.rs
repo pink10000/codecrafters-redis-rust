d873a4584c7655086fa06bfdbf955fe7d9ccf510
pink10000 <kyletrinh1223@gmail.com>
2024-12-20 21:32:01 -0800

    hex crate + decode hex

--- src/main.rs
+++ src/main.rs
@@ -48,9 +48,10 @@ fn handle_client(mut stream: TcpStream, srv: &Arc<Mutex<ServerState>>) {
         // if it does, then send it after the serialized response
         if serialized_response.contains("FULLRESYNC") {
             let full_resync = srv.lock().unwrap().full_resync();
-            // let _ = stream.write(full_resync.0.as_bytes());
-            let _ = stream.write(full_resync.1.as_bytes());
-            println!("Sent full resync: {:?}", full_resync);
+            let _ = stream.write(full_resync.0.as_bytes());
+            let rdb_bytes = hex::decode(full_resync.1).unwrap();
+            let _ = stream.write(&rdb_bytes);
+            // println!("Sent full resync: {:?}", full_resync);
         }
     }
 }
